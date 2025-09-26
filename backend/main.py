2b27ec6b4383950951887470a337331394799a35
Ibrahim Khan <ikhan@azmx.sa>
2025-09-26 18:58:11 +0530

    updated main

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
